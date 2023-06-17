b46acb66fbd71a36bfba3ae6224190da1049b4c4
maxwellpothier <pothiermaxwell@gmail.com>
2023-06-17 16:15:53 -0600

    parse the request body

--- handlers/identityHandlers.go
+++ handlers/identityHandlers.go
@@ -1,14 +1,28 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type NewUserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func CreateNewUserHandler(w http.ResponseWriter, r *http.Request) {
-	message := "Create User: /identity/create"
-    w.WriteHeader(http.StatusOK)
-    w.Write([]byte(message))
+	// Parse the request body into a NewUserRequest struct
+	var newUser NewUserRequest
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, "Could not read request body", http.StatusBadRequest)
+		return
+	}
+
+	message := fmt.Sprintf("Username: %s\nPassword: %s", newUser.Username, newUser.Password)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(message))
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
